ebe3a881c6d58e9f9a4915cd57392cbefbd19a56
Zaynab Khan <151539134+Khan1021@users.noreply.github.com>
2026-08-28 13:57:00 +0100

    animations for modeLabels completed

--- src/components/SessionInfo.jsx
+++ src/components/SessionInfo.jsx
@@ -11,7 +11,7 @@ function SessionInfo(props) {
         <>
             <div style={{ color: sessionColours[props.modeLabels] }}>
                 <div id="modeLabels">
-                    <h3>{props.modeLabels}</h3>
+                    <h3 key={props.modeLabels}>{props.modeLabels}</h3>
                 </div>
                 <h3>{props.positionInCycle}</h3>
                 <h2>{props.totalWorkSessions}</h2>
